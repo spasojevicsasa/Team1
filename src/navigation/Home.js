2f5735be95ea8b0f946924208e17b7ab16af0165
ivanapfafsovljanski <ivana.pfafsovljanski@gmail.com>
2021-06-29 10:16:14 +0200

    dodata struktura

---
+++ src/navigation/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Home extends React.Component{
+    render (){
+        return(
+            <div>
+                <h1>WELCOME</h1>
+                <h1>F1 CHAMPIONSHIP</h1>
+            </div>
+        )
+    }
+}
---
+++ src/navigation/Races.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Races extends React.Component{
+    render (){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
