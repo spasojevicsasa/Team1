00131977215362928170e2500b6ad91ede70ba49
Sasa Spasojevic <spsjvcsasa@gmail.com>
2021-06-30 08:51:16 +0200

    naslov

--- src/navigation/Home.js
+++ src/navigation/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-export default class Home extends React.Component{
-    render (){
-        return(
-            <div className="mainScreen">
-                <h1>WELCOME</h1>
-                <h1>F1 CHAMPIONSHIP</h1>
-            </div>
-        )
-    }
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div className="mainScreen">
+        <h1>WELCOME</h1>
+        <h1>F1 CHAMPIONSHIP</h1>
+        <h2>naslov</h2>
+      </div>
+    );
+  }
 }
